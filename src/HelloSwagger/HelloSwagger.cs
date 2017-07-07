1f86875fcdae4c188864d7718e01b0758173469d
xplicit <svg@ngs.ru>
2017-07-08 01:38:09 +0700

    Initial commit

---
+++ src/HelloSwagger/HelloSwagger.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Funq;
+using ServiceStack;
+using ServiceStack.Configuration;
+using ServiceStack.Host.Handlers;
+using ServiceStack.Redis;
+using ServiceStack.Api.Swagger;
+
+namespace Todos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseServiceStack(new AppHost());
+
+            app.Use(new RequestInfoHandler());
+        }
+    }
+
+    // Create your ServiceStack Web Service with a singleton AppHost
+    public class AppHost : AppHostBase
+    {
+        // Initializes your AppHost Instance, with the Service Name and assembly containing the Services
+        public AppHost() : base("Swagger Sample", typeof(HelloSwaggerService).GetAssembly()) 
+        { 
+        }
+
+        // Configure your AppHost with the necessary configuration and dependencies your App needs
+        public override void Configure(Container container)
+        {
+            Plugins.Add(new SwaggerFeature());
+        }
+    }
+
+    // Define your ServiceStack web service request (i.e. Request DTO).
+    [Route("/hello/{name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    // Create your ServiceStack rest-ful web service implementation. 
+    public class HelloSwaggerService : Service
+    {
+        public object Any(Hello request) => new HelloResponse { Result = "Hello, " + request.Name};
+    }
+}
+
